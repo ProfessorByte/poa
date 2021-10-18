3d6d329c9887327fee13ad96381cff05f6e3f073
mavl44 <maury.vargasl@gmail.com>
2021-10-17 20:45:33 -0400

    anadiendo /biblioteca a Repositorio

--- src/paginas/Repositorio.js
+++ src/paginas/Repositorio.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter, Route } from "react-router-dom";
 import Biblioteca from "./Biblioteca";
-class Repositorio extends React.Component {
-    render() { 
-        return <BrowserRouter>
-            <Route path="/biblioteca" Component={Biblioteca}></Route>
-        </BrowserRouter>
-    }
+function Repositorio() {
+ 
+        return (<BrowserRouter>
+            <Route path="/biblioteca" component ={Biblioteca}/>
+        </BrowserRouter>)
+
 }
  
 export default Repositorio;
