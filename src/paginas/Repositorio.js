c8a4a9281ca3749d8dcdbff03f71c6cbe381ec7f
mavl44 <maury.vargasl@gmail.com>
2021-10-17 21:01:51 -0400

    anadiendo boton en repositorio

--- src/paginas/Repositorio.js
+++ src/paginas/Repositorio.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 function Repositorio() {
  
         return (<div>
             repositorio
-
+            <Link to="">
+                <img src="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/add.png?alt=media&token=fbe1d973-4b4f-42d8-b76e-2c4354f6ed9d"
+                />
+            </Link>
         
         </div>)
 
