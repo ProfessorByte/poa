8f0875a5e5dc1e9290cf14536175219d34d07765
mavl44 <maury.vargasl@gmail.com>
2021-10-17 21:02:43 -0400

    anadiendo boton Link en Biblioteca

--- src/paginas/Biblioteca.js
+++ src/paginas/Biblioteca.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Repositorio from "./Repositorio";
+import { Link } from "react-router-dom";
 
 
 class Biblioteca extends React.Component {
@@ -8,7 +9,10 @@ class Biblioteca extends React.Component {
         return (
             <div>
                 Biblioteca
-                
+                <Link to="">
+                <img src="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/add.png?alt=media&token=fbe1d973-4b4f-42d8-b76e-2c4354f6ed9d"
+                />
+            </Link>
             </div>
         )
     }
