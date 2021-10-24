da36c7367c25c7fd8c7deedc0930d261d0cc493d
mavl44 <maury.vargasl@gmail.com>
2021-10-24 17:52:54 -0400

    arreglando tamano de botn

--- src/components/ButtonBackComponent.js
+++ src/components/ButtonBackComponent.js
@@ -5,7 +5,7 @@ import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
 
 export const ButtonBack = ({ dir }) => {
   return (
-    <Link className="eightbit-btn eightbit-btn--proceed col-md-4 col-md-1 col-3" to={dir}>
+    <Link className="eightbit-btn eightbit-btn--proceed col-md-1 col-3 btn-sm" to={dir}>
       <span>
         <FontAwesomeIcon icon={faLongArrowAltLeft} size="2x" />
       </span>
