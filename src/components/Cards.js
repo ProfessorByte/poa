b992668eecc36c58801cb59a69099c648af9ce50
Alejandroxp011 <87455252+Alejandroxp011@users.noreply.github.com>
2021-10-24 15:26:13 -0400

    Revert "Merge branch 'Arreglos_bugs_Bibliografia' of https://github.com/ProfessorByte/poa into Arreglos_bugs_Bibliografia"
    This reverts commit 271bfafd4b8b31d7382bc067336b5c13c3769c66, reversing
    changes made to f0f6ece2bcb971082b889ce220497013396bfc9a.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,22 +8,27 @@ import { Container } from "react-bootstrap";
 const Cards =(props)=>{
     const {tituloReferncia,temas,link,autor_NombrePagina,tipo,}=props
     return(
-        <div className="wrapper">
+        <div>
+            <Container>
+                <figure className="position-relative">
+                    <img src ={libroPNG} alt="libro" className="img-fluid" />
+                </figure >
+                <figcaption>
 
-
-            <div className="card" >
-                <div className="card-bodyCard">
-                    <h5 className="card-title">{tituloReferncia}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">{temas}</h6>
-                    <h7 className="card-title">{autor_NombrePagina}</h7>
-                    <br/>
-                    <h7 className="card-text">{tipo}</h7>
-                    <br/>
-                    <a href={link} target="_blank" className="card-link" style={{link}}> Link</a>
-                </div>
-            </div>
+                    <div className="card"  style={{border: "#FAF4F6"}}>
+                        <div className="card-bodyCard">
+                            <h5 className="card-title">{tituloReferncia}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{temas}</h6>
+                            <h7 className="card-title">{autor_NombrePagina}</h7>
+                            <br/>
+                            <h7 className="card-text">{tipo}</h7>
+                            <br/>
+                            <a href={link} target="_blank" className="card-link" style={{link}}> Link</a>
+                        </div>
+                    </div>
+                </figcaption>
+            </Container>
         </div>
-
 
     )
 }
