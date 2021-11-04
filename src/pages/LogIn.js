f2ff84a2070add834f37d7edcd7c47ac57c4b7ad
mavl44 <maury.vargasl@gmail.com>
2021-11-04 15:48:03 -0400

    creando pagina de Log In

---
+++ src/pages/LogIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LogIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1> Log In </h1> );
+    }
+}
+ 
+export default LogIn;
