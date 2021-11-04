7ef5c52bce4ec304614f6362b811456f11154ddb
mavl44 <maury.vargasl@gmail.com>
2021-11-04 16:11:58 -0400

    anadiendo campo para Email

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
-import {FacebookLoginButton} from 'react-social-login-buttons';
+import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { FacebookLoginButton } from "react-social-login-buttons";
 
 class LogIn extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {};
     }
-    render() { 
+    render() {
         return (
-        <div className="App">
-            LOGIN PAGE
-        </div>
-            );
+            <Form className="login-form">
+                <h1> LOGIN PAGE</h1>
+                <h2>Bienvenido de Vuelta</h2>
+                <FormGroup>
+                    <Label>EMAIL</Label>
+                    <Input type="email" placeholder="Email"/>
+                </FormGroup>
+
+                <Button className="btn-lg btn-dark btn-block">Log IN</Button>
+            </Form>
+        );
     }
 }
- 
+
 export default LogIn;
