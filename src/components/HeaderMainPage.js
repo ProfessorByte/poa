f35f7d4aaddb6f29cf19b7dd05be52da085c5574
Serboda <sebasm.sw@gmail.com>
2021-11-05 20:22:30 -0400

    HeaderMain

---
+++ src/components/HeaderMainPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../css/Headermain.css";
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-light navbar-expand-sm navbar-styles">
+          <div className="container-fluid d-flex justify-content-start">
+            <strong>
+              <h1>POA</h1>
+            </strong>
+            <div className="offset-lg-1 offset-3"></div>
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
