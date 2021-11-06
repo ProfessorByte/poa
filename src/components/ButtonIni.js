4507058550e9d4200627473d34baf89af74e70a0
Moritakha <92400582+Moritakha@users.noreply.github.com>
2021-11-05 22:52:07 -0400

    Boton inicio sesion

---
+++ src/components/ButtonIni.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/ButtonIni.css"
+const ButtonIni = () => {
+  return (
+  <div className="ContenedorB">
+  <button type="button" class="btn btn-light float-sm-right">Iniciar Sesion</button>
+  </div>
+  );
+};
+export default ButtonIni;
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { ButtonBack } from "./ButtonBackComponent";
 import "../App.css";
+import ButtonIni from "./ButtonIni";
 export default class Header extends Component {
   render() {
     return (
@@ -10,7 +11,10 @@ export default class Header extends Component {
             <ButtonBack dir="/poa" />
             <strong>
               <h1>POA</h1>
-            </strong>
+            </strong> 
+               <div className="botonIni">
+                <ButtonIni/> 
+               </div>
             <div className="offset-lg-1 offset-3"></div>
           </div>
         </nav>
