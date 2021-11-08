35513ff59e6b163a5b800feed24b8cbc2a897a9a
mavl44 <maury.vargasl@gmail.com>
2021-11-08 16:49:24 -0400

    anadiendo bordes rojos

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -4,11 +4,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const iniState = {
-        email : "",
-        password : "",
-        emailError : "",
-        passwordError : ""
-}
+    email: "",
+    password: "",
+    emailError: "",
+    passwordError: "",
+};
 
 class LogInComponent extends React.Component {
     constructor(props) {
@@ -16,105 +16,132 @@ class LogInComponent extends React.Component {
     }
 
     state = {
-        showPassword : false,
-        email : "",
-        password : "",
-        emailError : "",
-        passwordError : ""
-    }
+        showPassword: false,
+        email: "",
+        password: "",
+        emailError: "",
+        passwordError: "",
+    };
 
-    togglePasswordVisibility =() =>{
-        const {showPassword} = this.state;
-        this.setState({ showPassword : !showPassword});
-    }
+    togglePasswordVisibility = () => {
+        const { showPassword } = this.state;
+        this.setState({ showPassword: !showPassword });
+    };
 
-    validate =() =>{
-        let emailError="";
-        let passwordError="";
+    validate = () => {
+        let emailError = "";
+        let passwordError = "";
 
-        if(!this.state.email){
-            emailError="debe ingresar un email"
-        }else{
-            if(!this.state.email.includes("@")){
-                emailError = "correo invalido, no contiene @"
-            }else{
-                if(!this.state.password){
-                    passwordError="debe ingresar una contraseña"
-                }else{
-                    if(this.state.password < 8){
-                        passwordError="la contraseña es muy corta"
+        if (!this.state.email) {
+            emailError = "debe ingresar un email";
+        } else {
+            if (!this.state.email.includes("@")) {
+                emailError = "correo invalido, no contiene @";
+            } else {
+                if (!this.state.password) {
+                    passwordError = "debe ingresar una contraseña";
+                } else {
+                    if (this.state.password.length < 8) {
+                        passwordError = "la contraseña es muy corta";
                     }
                 }
             }
-    
         }
-        if(emailError || passwordError){
-            this.setState({emailError,passwordError});
+        if (emailError || passwordError) {
+            this.setState({ emailError, passwordError });
             return false;
         }
 
-        
         return true;
+    };
 
-    }
-
-    handleChangeEmail = event => {
-        this.setState({ email : event.target.value})
-    }
-    handleChangePassword = event => {
-        this.setState({ password : event.target.value})
-    }
-    handleSubmit = event =>{
+    handleChangeEmail = (event) => {
+        this.setState({ email: event.target.value });
+    };
+    handleChangePassword = (event) => {
+        this.setState({ password: event.target.value });
+    };
+    handleSubmit = (event) => {
         event.preventDefault();
         const isValid = this.validate();
         console.log(this.state);
-        if(isValid){
+        if (isValid) {
             console.log(this.state);
             //limpiar el form
             this.setState(iniState);
         }
-
-    }
+    };
 
     render() {
-        const { showPassword} = this.state;
-        
-        const ojo= {faEye};
+        const { showPassword } = this.state;
+        const ojo = { faEye };
         return (
-            <Form className="form-group formulario row" onSubmit={this.handleSubmit}>
+            <Form
+                className="form-group formulario row"
+                onSubmit={this.handleSubmit}
+            >
                 <h1 className="form-title"> LogIn</h1>
                 <div className="col">
                     <FormGroup className=" label">
                         <Label>Correo Electronico</Label>
-                        <Input
-                            type="text"
-                            placeholder="Email"
-                            className="rounded-pill"
-                            value={this.state.email}
-                            onChange={this.handleChangeEmail}
-                        />
+                        <div
+                            className={
+                                this.state.emailError
+                                    ? "rounded-pill wrong-input"
+                                    : "rounded-pill"
+                            }
+                        >
+                            <Input
+                                type="text"
+                                placeholder="Email"
+                                className="rounded-pill"
+                                value={this.state.email}
+                                onChange={this.handleChangeEmail}
+                            />
+                        </div>
                         <div className="mensaje-error">
                             {this.state.emailError}
                         </div>
                     </FormGroup>
+
                     <FormGroup className=" label">
                         <Label> Contraseña :</Label>
-                        <div className="input-password">
-                            <Input
-                                type= {(showPassword) ? "text" : "password"}
-                                placeholder="Contraseña"
-                                className="rounded-pill"
-                                value={this.state.password}
-                                onChange={this.handleChangePassword}
+                        <div className="input-password rounded-pill">
+                            <div
+                                className={
+                                    this.state.passwordError
+                                        ? "rounded-pill wrong-input"
+                                        : "rounded-pill"
+                                }
+                            >
+                                <Input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="Contraseña"
+                                    className="rounded-pill"
+                                    value={this.state.password}
+                                    onChange={this.handleChangePassword}
+                                />
+                            </div>
+                            <FontAwesomeIcon
+                                icon={faEye}
+                                size="2x"
+                                className={
+                                    showPassword
+                                        ? "icon open-eye rounded-pill"
+                                        : "icon rounded-pill"
+                                }
+                                onClick={this.togglePasswordVisibility}
                             />
-                            <FontAwesomeIcon icon={faEye} size="2x" className={(showPassword) ? "icon white-eye" : "icon"} onClick={this.togglePasswordVisibility} />
                         </div>
                         <div className="mensaje-error">
                             {this.state.passwordError}
                         </div>
                     </FormGroup>
                     <div className="label form-btn">
-                        <Button type="submit" className="btn btn-light btn-lg rounded-pill" >
+                        <Button
+                            type="submit"
+                            className="btn btn-light btn-lg rounded-pill"
+                        >
                             Ingresar
                         </Button>
                     </div>
