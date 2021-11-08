2c6ba5a27bd63079e45f2979d8a4a4c304e5f07f
mavl44 <maury.vargasl@gmail.com>
2021-11-07 23:24:08 -0400

    arreglando diseno y quitando boton de facebook

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -18,19 +18,13 @@ class LogInComponent extends React.Component {
                         <Input type="email" placeholder="Email" className="rounded-pill"/>
                     </FormGroup>
                     <FormGroup className=" label">
-                        <Label> Contraseña</Label>
+                        <Label> Contraseña :</Label>
                         <Input type="password" placeholder="Contraseña" className="rounded-pill"/>
                     </FormGroup>
-                    <div className="label">
-                        <Button className="btn btn-info btn-lg rounded-pill">
+                    <div className="label form-btn">
+                        <Button className="btn btn-light btn-lg rounded-pill">
                             Ingresar
                         </Button>
-                    </div>
-                    <div className="text-center pt-3 label">
-                        O continua con tu cuenta social
-                    </div>
-                    <div className="">
-                        <FacebookLoginButton className="mt-3 mb-3" />
                     </div>
                 </div>
 
