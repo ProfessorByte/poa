730ad6a931e6a31a4f7a2bf21ccf3d4af0e37aeb
JoDemVel <josuedemetriovelizescobar@gmail.com>
2021-11-08 22:11:53 -0400

    Se puso dentro de un flex los cards junto con un onClick

--- src/components/CardNivel.js
+++ src/components/CardNivel.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default function CardNivel() {
   return (
-    <div className="card">
-      <div className="card-body">
-        <h4 className="card-title">Titulo x</h4>
-        <p className="card-text">Pruebita de card simple</p>
+    <div onClick={() => alert("Hello from here")}>
+      <div className="card text-white bg-success mb-3">
+        <div class="card-header">Titulillo 1</div>
+        <div className="card-body">
+          <h4 className="card-title">Subtitulillo 1</h4>
+          <p className="card-text">Pruebita de card simple</p>
+        </div>
       </div>
     </div>
   )
--- src/components/CardsNivel.js
+++ src/components/CardsNivel.js
@@ -3,12 +3,21 @@ import CardNivel from './CardNivel'
 
 export default function CardsNivel() {
   return (
-    <div className="container">
+    <div className="container flex-container">
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-3 flex">
           <CardNivel></CardNivel>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3 flex">
+          <CardNivel></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
+          <CardNivel></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
+          <CardNivel></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
           <CardNivel></CardNivel>
         </div>
       </div>
