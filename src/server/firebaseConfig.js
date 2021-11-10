dfa4c52c15bcce713a27440a22966736fab51e89
Pablo Pardo <pablopardoalcocer@gmail.com>
2021-11-10 11:16:35 -0400

    Initial configuration for firebase

--- src/server/firebaseConfig.js
+++ src/server/firebaseConfig.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -17,9 +11,8 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore();
+const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth(firebaseApp);
 
-export default db;
+export default firebaseApp;
