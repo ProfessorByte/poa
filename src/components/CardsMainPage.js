42289a1a3f07579e2cdf15e6b58f81e11f459979
mary173 <mariananatv@gmail.com>
2021-11-09 20:52:56 -0400

    animacion de las imagenes de las cards de la pagina principal

--- src/components/CardsMainPage.js
+++ src/components/CardsMainPage.js
@@ -1,17 +1,21 @@
 import React from "react";
 import '../css/PaginaPrincipal.css'
+import ReactDOM from "react-dom";
+
+
 
 const Cards =(props)=>{
-    const {imagen,title,descripcion}=props
+    const {imagen,title,descripcion,funcion}=props
     return(
-        <div className="card text-black bg-secondary mb-3" >
-            <h5 className="card-title text-center tamaño">{title}</h5>
-            <img src={imagen} className="card_main_imagen" alt="..."></img>
+        <div className="card text-black bg-secondary mb-3 tamaño_card ">
+            <h5 className="card-title text-center tamaño ">{title}</h5>
+            <img src={imagen} onClick={funcion} className="card_main_imagen  p-3  " alt="..."></img>
             <div className="card-body">
-                <p className="card-text text-center">{descripcion}</p>
+                <p className="card-text text-center mintamaño">{descripcion}</p>
             </div>
         </div>
 
     )
 }
-export default Cards
+
+export default Cards;
--- src/pages/PaginaPrincipalPage.js
+++ src/pages/PaginaPrincipalPage.js
@@ -4,24 +4,29 @@ import Cards from "../components/CardsMainPage";
 import "../css/PaginaPrincipal.css";
 import imagenEspada from "../assets/images/espadas.png";
 import imagenLibro from "../assets/images/libro (200 x 200 px).png";
+import { Link } from "react-router-dom";
+
+function pressFirstImage(e) {
+    alert("Inicie seción si desea guardar los cambios de historia ");
+}
+function pressSecondImage(e) {
+    window.open("/poa" );
+
+}
 export default function PaginaPrincipalPage() {
     return (
         <div className="fondoPaginaPrincipal">
             <div>
-                <div>
-                    <Header/>
-                </div>
 
-                <div className="container d-flex align-items-center justify-content-center vh-100 vw-100">
+                <div className="container d-flex align-items-center justify-content-center vh-100 vw-100 ">
                     <div className="row">
                         <div className="col-6">
 
                             <Cards
                                 title="Historia"
                                 imagen={imagenEspada}
-                                descripcion="¡Entra qui para explorar los diferentes niveles que tenemos listos para ti para que puedas aprender los temas !"
-
-
+                                funcion={pressFirstImage}
+                                descripcion="¡Entra aquí para explorar los diferentes niveles que tenemos listos para ti para que puedas aprender los temas !"
                             />
                         </div>
                         <div className="col-6">
@@ -30,8 +35,8 @@ export default function PaginaPrincipalPage() {
                                 title="Repositorio"
                                 descripcion="¡Entra aquí para poder reforzar todos los conocimentos que aprenderas en la historia!"
                                 imagen={imagenLibro}
+                                funcion={pressSecondImage}
                             />
-
                         </div>
                     </div>
                 </div>
