2dbe7c883142b65bee52afb4d25c3e4496c03438
mavl44 <maury.vargasl@gmail.com>
2021-11-11 16:25:10 -0400

    dando diseno al modal

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -32,26 +32,39 @@ class ModalComponent extends React.Component {
                     aria-labelledby="tituloVentana"
                     arial-hidden="true"
                 >
-                    <div className="modal-dialog" role="document">
+                    <div className="modal-dialog modal-lg" role="document">
                         <div
-                            className="modal-content"
+                            className="modal-content "
                             style={{ color: "black" }}
                         >
                             <div className="modal-header">
                                 <h1 id="tituloVentanta"> Titulo Modal</h1>
                                 <button
-                                    className="close btn-warning"
+                                    className="close btn-warning col-1"
                                     data-bs-dismiss="modal"
                                     aria-label="cerrar"
                                 >
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                            <div className="modal-body">
+                            <div className="modal-body myModal">
                                 <div className="alert alert-success">
-                                    <h4> Cuerpo del modal</h4>
-                                    <div>
+                                    <div className="myModalimg">
                                         <img src="../assets/images/start.jpg"></img>
+                                    </div>
+                                    <div>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore et dolore magna
+                                        aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.
+                                        Duis aute irure dolor in reprehenderit
+                                        in voluptate velit esse cillum dolore eu
+                                        fugiat nulla pariatur. Excepteur sint
+                                        occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim
+                                        id est laborum.
                                     </div>
                                 </div>
                             </div>
