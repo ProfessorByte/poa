5fc878c87e524604a3f269a327c8ef6436dfa0fd
Moritakha <92400582+Moritakha@users.noreply.github.com>
2021-11-10 20:23:50 -0500

    Icono de usuario en el boton

--- src/components/BotonIcono.js
+++ src/components/BotonIcono.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../css/ButtonsUser.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 const BotonIcono = () => {
+  <link rel="stylesheet" href="css/font-awesome.min.css"></link>
   return (
     <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Usuario
+    <FontAwesomeIcon icon={faUser} size="2.5x" /> Usuario
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="#">Action</a>
