c48f0182e054bb33f0b174e5898188b0a29ada59
JoDemVel <josuedemetriovelizescobar@gmail.com>
2021-11-10 21:58:31 -0400

    Props añadidos respecto al subtitilo de cada nivel

--- src/components/CardNivel.js
+++ src/components/CardNivel.js
@@ -1,25 +1,30 @@
 import React from 'react'
 
-export default function CardNivel({ estado }) {
+export default function CardNivel({ estado, titulo, desc }) {
   console.log({estado});
   const getColor = (n) => {
       var color = "";
       if( estado === -1 ){
-        color = "card text-white bg-secondary mb-3";
+        color = "card text-white bg-secondary mb-3 ";
       }else if(estado === 0){
-        color = "card text-white bg-warning mb-3";
+        color = "card text-white bg-warning mb-3 ";
       }else{
-        color = "card text-white bg-success mb-3";
+        color = "card text-white bg-success mb-3 ";
       }
       return color;
     }
   return (
-    <div onClick={() => alert("Hello from here")}>
+    <div >
       <div className={getColor(estado)}>
-        <div class="card-header">Titulillo 1</div>
+        <div className="card-header">{titulo}</div>
         <div className="card-body">
-          <h4 className="card-title">Subtitulillo 1</h4>
-          <p className="card-text">Pruebita de card simple</p>
+          <p className="card-text">{desc}</p>
+          <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+            <div className="btn-group mr-5" role="group" aria-label="First group">
+              <button type="button" className="btn btn-light" onClick={() => alert("Hello from here")}>Comenzar</button>
+              <button type="button" className="btn btn-info" onClick={() => alert("Hello from here")}>Info</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/CardsNivel.js
+++ src/components/CardsNivel.js
@@ -6,19 +6,34 @@ export default function CardsNivel() {
     <div className="container flex-container">
       <div className="row">
         <div className="col-md-3 flex">
-          <CardNivel estado={1}></CardNivel>
+          <CardNivel estado={1} titulo = "Objetos y Clases" desc = "Tipos de datos"></CardNivel>
         </div>
         <div className="col-md-3 flex">
-          <CardNivel estado={0}a></CardNivel>
+          <CardNivel estado={0} titulo="Abstraccion"></CardNivel>
         </div>
         <div className="col-md-3 flex">
-          <CardNivel estado={-1}b></CardNivel>
+          <CardNivel estado={-1} titulo="Encapsulamiento" desc="Tipos de Modificadores de Acceso"></CardNivel>
         </div>
         <div className="col-md-3 flex">
-          <CardNivel estado={-1}></CardNivel>
+          <CardNivel estado={-1} titulo="Encapsulamiento" desc="Atributos"></CardNivel>
         </div>
         <div className="col-md-3 flex">
-          <CardNivel estado={-1}></CardNivel>
+          <CardNivel estado={-1} titulo="Encapsulamiento" desc="Metodos"></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
+          <CardNivel estado={-1} titulo="Encapsulamiento" desc="Constructores"></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
+          <CardNivel estado={-1} titulo="Encapsulamiento" desc="Variables locales y globales"></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
+          <CardNivel estado={-1} titulo="Definicio de Clases" desc="Modificador static y final"></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
+          <CardNivel estado={-1} titulo="Herencia"></CardNivel>
+        </div>
+        <div className="col-md-3 flex">
+          <CardNivel estado={-1} titulo="Polimorfismo"></CardNivel>
         </div>
       </div>
     </div>
