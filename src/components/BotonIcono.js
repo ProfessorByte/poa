2363d29a6d8b1f4207009cfab38e50cd84aabd27
Moritakha <92400582+Moritakha@users.noreply.github.com>
2021-11-12 09:55:29 -0500

    Boton dropdown de usuario

--- src/components/BotonIcono.js
+++ src/components/BotonIcono.js
@@ -6,13 +6,13 @@ const BotonIcono = () => {
   <link rel="stylesheet" href="css/font-awesome.min.css"></link>
   return (
     <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <FontAwesomeIcon icon={faUser} size="2.5x" /> Usuario
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <FontAwesomeIcon icon={faUser} size="2.5x" />
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
+    <a class="dropdown-item" href="#">Portugal</a>
+    <a class="dropdown-item" href="#">Darse de baja</a>
+    <a class="dropdown-item" href="#">Cerrar Sesion </a>
   </div>
 </div>
   );
