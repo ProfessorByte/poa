422f53bec6ef407145ea7a20a2a29922c81b365a
iveliz <ayalaiveliz@gmail.com>
2021-11-13 17:53:15 -0400

    Create HistoriaPage

---
+++ src/pages/Historia.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import { Parallax, Background } from "react-parallax";
+
+export default function Historia (){
+  return( 
+  <div>
+    <h1>Hola</h1>
+  </div>
+
+  )
+}
+
