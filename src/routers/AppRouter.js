bba7cccc412e21ffa6cf89ff0af6a5a4271ce540
iveliz <ayalaiveliz@gmail.com>
2021-11-14 12:10:39 -0400

    RouterPageHistoria

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from "react-router";
 import BibliografiaPage from "../pages/BibliografiaPage";
 import Repositorio from "../pages/Repositorio";
 import VideosPage from "../pages/VideosPage";
+import Historia from "../pages/Historia";
 
 
 export default function AppRouter() {
@@ -12,6 +13,7 @@ export default function AppRouter() {
         <Route exact path="/poa" component={Repositorio} />
         <Route exact path="/poa/videos" component={VideosPage} />
         <Route exact path="/poa/bibliografia" component={BibliografiaPage} />
+        <Route exact path="/poa/Historia" component={Historia}/>
         <Redirect to="/poa" />
       </Switch>
     </>
