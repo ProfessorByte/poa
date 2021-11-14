8121daa35f0bce196898f50775ce8f9b56cbc3c7
JoDemVel <josuedemetriovelizescobar@gmail.com>
2021-11-14 12:08:59 -0400

    Se añadio lectura de un archivo json para los niveles

--- src/components/CardNivel.js
+++ src/components/CardNivel.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function CardNivel({ estado, titulo, desc, num }) {
-  console.log({estado});
   const getColor = (n) => {
       var color = "";
       if( estado === -1 ){
@@ -19,7 +18,7 @@ export default function CardNivel({ estado, titulo, desc, num }) {
         {/*<div className="card-header text-center">{titulo}</div>*/}
         <div className="card-body">
           <div className="card-header">{num}</div>
-          <h6 className="card-text">{desc}</h6>
+          <h6 className="card-text">{titulo}</h6>
           <button type="button" className="btn btn-light btn-sm btn-block" onClick={() => alert("Hello from here")}>start</button>
         </div>
       </div>
--- src/components/CardsNivel.js
+++ src/components/CardsNivel.js
@@ -1,59 +1,47 @@
 import React from 'react'
 import CardNivel from './CardNivel'
+import Nivs from "../nivs.json";
 
 export default function CardsNivel() {
+  
+  const closedIntervalNivs = (listNivs, minId,maxId) => {
+    return listNivs.filter(
+      (nivel) => nivel.id >= minId && nivel.id <= maxId
+    );
+  }
+
   return (
-    <div>
-      <h2>Objetos y Clases:</h2>
+    <div> 
+      <h2>Objetos y Clases:</h2> 
       <div className="container flex-container">
         <div className="row">
-          <div className="col-md-3 flex">
-            <CardNivel estado={1} desc = "Parametros" num="01"></CardNivel>
-          </div>
-          <div className="col-md-3 flex">
-            <CardNivel estado={0} desc ="Tipos de Datos" num="02"></CardNivel>
-          </div>
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="Abstraccion" num="03"></CardNivel>
-          </div>
+            { closedIntervalNivs(Nivs,1,2).map(post => {
+                return (
+                  <div className="col-md-3 flex">
+                    <CardNivel estado={post.estado} titulo = {post.titulo} num={post.id}></CardNivel>
+                  </div>
+                )
+              }) 
+            }
         </div>
       </div>
       <h2>Encapsulamiento:</h2>
       <div className="container flex-container">
         <div className="row">
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="Tipos de Modificadores de Acceso" num="04"></CardNivel>
-          </div>
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="Modificador static y final" num="05"></CardNivel>
-          </div>
+            { closedIntervalNivs(Nivs,3,3).map(post => {
+                return (
+                  <div className="col-md-3 flex">
+                    <CardNivel estado={post.estado} titulo = {post.titulo} num={post.id}></CardNivel>
+                  </div>
+                )
+              }) 
+            }
         </div>
       </div>
       <h2>Definiciones de Clases:</h2>
-      <div className="container flex-container">
-        <div className="row">
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="Conversion ascendente y descendente de tipos" num="06"></CardNivel>
-          </div>
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="Metodos" num="07"></CardNivel>
-          </div>
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="Constructores" num="08"></CardNivel>
-          </div>
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="Variables locales y globales" num="09"></CardNivel>
-          </div>
-        </div>
-      </div>
+      
       <h2>Estructuras de Control:</h2>
-      <div className="container flex-container">
-        <div className="row">
-          <div className="col-md-3 flex">
-            <CardNivel estado={-1} desc="If-Else" num="10"></CardNivel>
-          </div>
-        </div>
-      </div>
+      
       <h2>Interaccion de objetos:</h2>
     </div>
   )
