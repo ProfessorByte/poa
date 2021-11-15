25668e0d56300762d46779e633379247b6a57caf
Pablo Pardo <pablopardoalcocer@gmail.com>
2021-11-14 23:22:38 -0400

    Add basic functionality to modal

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -1,94 +1,109 @@
 import React from "react";
 
-import Figura from "../assets/images/start.jpg";
+import startImage from "../assets/images/start.jpg";
 
 class ModalComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showModal: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
 
-    handleModal() {
-        this.setState({ showModal: !this.state.showModal });
-    }
+  handleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
 
-    render() {
-        return (
-            <div>
+  render() {
+    return (
+      <>
+        <button
+          className="btn btn-success"
+          data-bs-toggle="modal"
+          data-bs-target="#ventanaModal"
+        >
+          Abrir Modal
+        </button>
+        <div
+          className="modal fade"
+          id="ventanaModal"
+          tabIndex={-1}
+          role="dialog"
+          aria-labelledby="tituloVentana"
+          arial-hidden={true}
+        >
+          <div className="modal-dialog modal-xl" role="document">
+            <div className="modal-content " style={{ color: "black" }}>
+              <div className="modal-header">
+                <h1 id="tituloVentanta">Nombre subtema</h1>
                 <button
-                    className="btn btn-success"
-                    data-bs-toggle="modal"
-                    data-bs-target="#ventanaModal"
-                >
-                    Abrir Modal
-                </button>
-                <div
-                    className="modal fade"
-                    id="ventanaModal"
-                    tabindex="-1"
-                    role="dialog"
-                    aria-labelledby="tituloVentana"
-                    arial-hidden="true"
-                >
-                    <div className="modal-dialog modal-lg" role="document">
-                        <div
-                            className="modal-content "
-                            style={{ color: "black" }}
-                        >
-                            <div className="modal-header">
-                                <h1 id="tituloVentanta"> Titulo Modal</h1>
-                                <button
-                                    className="close btn-warning col-1"
-                                    data-bs-dismiss="modal"
-                                    aria-label="cerrar"
-                                >
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body myModal">
-                                <div className="alert alert-success">
-                                    <div className="myModalimg">
-                                        <img src="../assets/images/start.jpg"></img>
-                                    </div>
-                                    <div>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipiscing elit, sed do eiusmod tempor
-                                        incididunt ut labore et dolore magna
-                                        aliqua. Ut enim ad minim veniam, quis
-                                        nostrud exercitation ullamco laboris
-                                        nisi ut aliquip ex ea commodo consequat.
-                                        Duis aute irure dolor in reprehenderit
-                                        in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur. Excepteur sint
-                                        occaecat cupidatat non proident, sunt in
-                                        culpa qui officia deserunt mollit anim
-                                        id est laborum.
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button
-                                    className="btn btn-warning"
-                                    type="button"
-                                    data-bs-dismiss="modal"
-                                >
-                                    Cerrar
-                                </button>
-                                <button
-                                    className="btn btn-succesa"
-                                    type="button"
-                                >
-                                    Aceptar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body myModal">
+                <div className="text-center">
+                  <img
+                    src={startImage}
+                    alt="Imágen de inicio"
+                    className="rounded"
+                  ></img>
                 </div>
+                <hr />
+                <div
+                  id="carouselStoryGame"
+                  className="carousel slide carousel-fade"
+                  data-bs-ride="carousel"
+                >
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <div className="container">
+                        La historia comienza con...
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      Bla bla bla fjevkankqanjkwvejn fkqenjkqefe eqjonqefkjb
+                      jknfewjknfqejkfq fwqjwnjewk
+                    </div>
+                    <div class="carousel-item">
+                      Más bla bla bla jnewiknfkjwe feoqn o3hdiqwifqwinjkfq
+                      njkfenjkww jnjknkj
+                    </div>
+                  </div>
+                  <button
+                    class="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselStoryGame"
+                    data-bs-slide="prev"
+                  >
+                    <span
+                      class="carousel-control-prev-icon"
+                      aria-hidden={true}
+                    ></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button
+                    class="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselStoryGame"
+                    data-bs-slide="next"
+                  >
+                    <span
+                      class="carousel-control-next-icon"
+                      aria-hidden={true}
+                    ></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default ModalComponent;
