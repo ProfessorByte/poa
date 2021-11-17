7acf342b9321ea2797abcdd96225e6005696aa78
Pablo Pardo <pablopardoalcocer@gmail.com>
2021-11-17 12:12:14 -0400

    Fix warnings

--- src/components/HeaderLogIn.js
+++ src/components/HeaderLogIn.js
@@ -1,20 +1,17 @@
 import React, { Component } from "react";
-import  "../css/HeaderMain.css"
-import ButtonIni from "./ButtonIni";
-import {ButtonBack} from "./ButtonBackComponent"
-import "../css/ButtonsUser.css"
-import Logo from "../assets/images/logo1.png"
+import "../css/HeaderMain.css";
+import "../css/ButtonsUser.css";
+import Logo from "../assets/images/logo1.png";
 export default class Header extends Component {
   render() {
     return (
       <>
         <nav className="navbar navbar-light navbar-expand-sm navLogIn">
-      
           <div className="container-fluid d-flex justify-content-between me-1 ms-1">
-          <a href="/poa">
-          <img id="logo2" src={Logo} width="150px" alt=""></img>   
-          </a>
-        </div>
+            <a href="/poa">
+              <img id="logo2" src={Logo} width="150px" alt=""></img>
+            </a>
+          </div>
         </nav>
       </>
     );
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,7 +5,6 @@ import ModalComponent from "../components/ModalComponent";
 import BibliografiaPage from "../pages/BibliografiaPage";
 import Historia from "../pages/Historia";
 import LogIn from "../pages/LogIn";
-import Niveles from "../pages/Niveles";
 import PaginaPrincipalPage from "../pages/PaginaPrincipalPage";
 import Repositorio from "../pages/Repositorio";
 import VideosPage from "../pages/VideosPage";
@@ -24,7 +23,11 @@ export default function AppRouter() {
         <Route exact path="/poa" component={PaginaPrincipalPage} />
         <Route exact path="/poa/repositorio" component={Repositorio} />
         <Route exact path="/poa/repositorio/videos" component={VideosPage} />
-        <Route exact path="/poa/repositorio/bibliografia" component={BibliografiaPage} />
+        <Route
+          exact
+          path="/poa/repositorio/bibliografia"
+          component={BibliografiaPage}
+        />
         <Route exact path="/poa" component={Repositorio} />
         <Route exact path="/poa/videos" component={VideosPage} />
         <Route exact path="/login">
@@ -32,8 +35,12 @@ export default function AppRouter() {
         </Route>
         <Route exact path="/niveles" component={ModalComponent} />
         <Route exact path="/poa/bibliografia" component={BibliografiaPage} />
-        <Route exact path="/poa/Historia" component={Historia}/>
-        <Route exact path="/PaginaPrincipalPage" component={PaginaPrincipalPage}/>
+        <Route exact path="/poa/Historia" component={Historia} />
+        <Route
+          exact
+          path="/PaginaPrincipalPage"
+          component={PaginaPrincipalPage}
+        />
         <Redirect to="/poa" />
       </Switch>
     </>
