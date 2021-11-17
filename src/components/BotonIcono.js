49d34b26651303e42693610fb61d3ddb7800315e
Moritakha <92400582+Moritakha@users.noreply.github.com>
2021-11-17 10:26:23 -0500

    imagen para el icono de usuario

--- src/components/BotonIcono.js
+++ src/components/BotonIcono.js
@@ -2,13 +2,14 @@ import React from "react";
 import "../css/ButtonsUser.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import Palta from "../assets/images/palta.jpg"
 const BotonIcono = () => {
   <link rel="stylesheet" href="css/font-awesome.min.css"></link>
   return (
     <div class="dropdown dropstart">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <FontAwesomeIcon icon={faUser} size="2.0x" />
-  </button>
+    <img id="iconu" src={Palta}  alt=""></img> 
+    </button>
   <div class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="#">Portugal</a>
     <a class="dropdown-item" href="#">Darse de baja</a>
