512428cc6561b397f6e74d843edb27e75fa6b77a
Moritakha <92400582+Moritakha@users.noreply.github.com>
2021-11-17 22:19:58 -0500

    Imagenes agregadas para el icono de usuario

--- src/components/BotonIcono.js
+++ src/components/BotonIcono.js
@@ -2,19 +2,35 @@ import React, {useState} from "react";
 import "../css/ButtonsUser.css"
 /*import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";*/
-import Jake from "../assets/images/jake.png"
-import Bender from "../assets/images/bender.png"
-import Palta from "../assets/images/palta.png"
-import Platano from "../assets/images/platano.png"
+import Abeja from "../assets/Avatars_poa/abejitas.PNG"
+import Ballena from "../assets/Avatars_poa/BallenaTumblr.PNG"
+import Lampara from "../assets/Avatars_poa/Lampara.PNG"
+import Librito from "../assets/Avatars_poa/librito.PNG"
+import Mariposa from "../assets/Avatars_poa/MariposaTraicionera.PNG"
+import Mochila from "../assets/Avatars_poa/mochilaRosas.PNG"
+import Poscion from "../assets/Avatars_poa/posicion magica.PNG"
+import Prt from "../assets/Avatars_poa/prt.PNG"
+import Submarino from "../assets/Avatars_poa/submarino.PNG"
+import TocaDiscos from "../assets/Avatars_poa/TocaDiscos.PNG"
+import Torre from "../assets/Avatars_poa/Torre.PNG"
+import Rafael from "../assets/Avatars_poa/Tortuguita.PNG"
 /*import cambiar from "./CambioIconos"*/
 const BotonIcono = () => {
-  const [icono, setIcono] = useState(Bender)
+  const [icono, setIcono] = useState(Abeja)
   const imagenAleatoria=()=>{
     var url = new Array();
-    url [0]= Bender;
-    url [1]= Jake;
-    url [2]= Palta;
-    url [3]= Platano;
+    url [0]= Abeja;
+    url [1]= Ballena;
+    url [2]= Lampara;
+    url [3]= Librito;
+    url [4]= Mariposa;
+    url [5]= Mochila;
+    url [6]= Poscion;
+    url [7]= Prt;
+    url [8]= Submarino;
+    url [9]= TocaDiscos;
+    url [10]= Torre;
+    url [11]= Rafael;
     var azar = Math.floor(Math.random() * url.length);
     return url[azar];
   }
