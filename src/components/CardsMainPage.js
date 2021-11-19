0853f493d51c134b516585559a3f3b347c187b5e
mary173 <mariananatv@gmail.com>
2021-11-19 10:53:11 -0400

    merge titulo y pagina

--- src/components/CardsMainPage.js
+++ src/components/CardsMainPage.js
@@ -1,23 +1,7 @@
 import React from "react";
 import "../css/PaginaPrincipal.css";
 
-const Cards = (props) => {
-  const { imagen, title, descripcion, funcion } = props;
-  return (
-    <div className="card text-black bg-secondary mb-3 tamaño_card ">
-      <h5 className="card-title text-center tamaño ">{title}</h5>
-      <img
-        src={imagen}
-        onClick={funcion}
-        className="card_main_imagen  p-3  "
-        alt="..."
-      ></img>
-      <div className="card-body card-bodyMain">
-        <p className="card-text text-center mintamaño">{descripcion}</p>
-      </div>
-    </div>
-  );
-};
+
 
 
 const Cards =(props)=>{
