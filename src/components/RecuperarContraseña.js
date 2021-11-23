8eb722169483a95ea33e9f1163cdbaebf2b8cc2a
Alejandroxp011 <87455252+Alejandroxp011@users.noreply.github.com>
2021-11-22 21:42:52 -0400

    borrada importacion innecesaria

--- src/components/RecuperarContraseña.js
+++ src/components/RecuperarContraseña.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
-import { signInWithEmailAndPassword } from "firebase/auth";
+
 const auth = getAuth();
 const iniState = {
   email: "",
