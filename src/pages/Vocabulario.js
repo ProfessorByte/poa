e8293fdda8be89249e9d30048f6b63321913b1cf
mavl44 <maury.vargasl@gmail.com>
2021-11-26 19:51:02 -0400

    creando input y anadiendo JSON

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -1,5 +1,8 @@
+import React from "react";
+import JSONDATA from '../assets/MOCK_DATA.json'
+
 export default function Vocabulario() {
     return ( <div>
-        
+        <input type="text" placeholder="Buscar..."></input>
     </div> );
 }
