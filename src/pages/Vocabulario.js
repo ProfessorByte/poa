71a1800790d83815f2fafb3b4808c3b4baecefd2
mary173 <mariananatv@gmail.com>
2021-11-25 23:51:34 -0400

    update cabezera

---
+++ src/pages/Vocabulario.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/HeaderVideos&Biblio";
+export default function Vocabulario() {
+    return (
+      
+            <div>
+              <Header />
+              
+              </div>
+    );
+
+}
