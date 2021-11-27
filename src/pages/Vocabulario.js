9982e337163e174fdc684e9f32579ffb5b0eafea
mary173 <mariananatv@gmail.com>
2021-11-27 16:03:40 -0400

    añadiendo cuadro de busqueda

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -3,30 +3,47 @@ import Header from "../components/HeaderVideos&Biblio";
 import "../css/Vocabulario.css"
 import FooterVocabulario from "../components/FooterRepositorio";
 import FrameVocabulario from "../components/Frame_Vocabulario";
+import { MDBCol, MDBIcon } from "mdbreact";
 export default function Vocabulario() {
- 
+
   return (
 
     <div>
       <Header />
       <div className="fondoVocabulario">
-        
-          <div className="content-wrap mb-3">
-            <div className="container">
-              <div className="row mt-3">
-                <div className="col">
-                  <h1>Vocabulario</h1>
-                </div>
+        <div className="content-wrap mb-3">
+          <div className="container">
+            <div className="row mt-3">
+              <div className="col">
+                <h1>Vocabulario</h1>
               </div>
             </div>
-            <FrameVocabulario />
+            <div class ="row mb-5">
+              <MDBCol md="6">
+                <div className="input-group md-form form-sm form-1 pl-0">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text purple lighten-3" id="basic-text1">
+                      <MDBIcon className="text-white" icon="search" />
+                    </span>
+                  </div>
+                  <input className="form-control my-0 py-1" type="text" placeholder="Buscar" aria-label="Search" />
+                </div>
+              </MDBCol>
+              </div>
+            <div>
+              <FrameVocabulario />
+            </div>
+            
           </div>
-          <FooterVocabulario />
-       
-
-
         </div>
-      </div>
-      );
-  }
+        <FooterVocabulario />
+    </div>
+    </div>
+        );
+}
+
+
+
+
+
 
