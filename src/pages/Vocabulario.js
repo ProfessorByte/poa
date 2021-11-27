a166f477f6e1e7c8f4bc4a07462b4f7cfa4486f4
mavl44 <maury.vargasl@gmail.com>
2021-11-26 20:05:46 -0400

    anadiendo constantes e useState

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -1,8 +1,20 @@
 import React from "react";
 import JSONDATA from '../assets/MOCK_DATA.json'
+import { ButtonBack } from "../components/ButtonBackComponent";
 
 export default function Vocabulario() {
     return ( <div>
         <input type="text" placeholder="Buscar..."></input>
+
+        {JSONDATA.map((val,key) => {
+            return <div>
+                {
+                    <div> 
+                         <button>val.first_name</button>
+                    </div>
+                   
+                }
+            </div>
+        })}
     </div> );
 }
