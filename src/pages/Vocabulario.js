85eb7e11697e7c99851682076d51cbacadc61894
mary173 <mariananatv@gmail.com>
2021-11-27 12:43:37 -0400

    añadiendo boton vocabulario

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/HeaderVideos&Biblio";
+import "../css/Vocabulario.css"
 export default function Vocabulario() {
-    return (
-      
-            <div>
-              <Header />
-              
-              </div>
-    );
+  return (
+
+    <div>
+      <Header />
+      <div className="fondoVocabulario">
+
+      </div>
+
+    </div>
+  );
 
 }
