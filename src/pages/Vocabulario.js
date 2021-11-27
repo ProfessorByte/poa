d339f9a25eef7b3aeba46c77a3da8b9d3daa5030
mary173 <mariananatv@gmail.com>
2021-11-27 13:49:18 -0400

    fixing vocabularioPage

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -2,16 +2,31 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/HeaderVideos&Biblio";
 import "../css/Vocabulario.css"
 import FooterVocabulario from "../components/FooterRepositorio";
+import FrameVocabulario from "../components/Frame_Vocabulario";
 export default function Vocabulario() {
+ 
   return (
 
     <div>
       <Header />
       <div className="fondoVocabulario">
+        <div className="page-container">
+          <div className="content-wrap mb-3">
+            <div className="container">
+              <div className="row mt-3">
+                <div className="col">
+                  <h1>Vocabulario</h1>
+                </div>
+              </div>
+            </div>
+            <FrameVocabulario />
+          </div>
+          <FooterVocabulario />
+        </div>
 
+
+        </div>
       </div>
-     <FooterVocabulario/>
-    </div>
-  );
+      );
+  }
 
-}
