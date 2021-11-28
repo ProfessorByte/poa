8994156738cbdbfca35680ff375faf3c8160913a
mavl44 <maury.vargasl@gmail.com>
2021-11-27 21:21:17 -0400

    local change

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -1,16 +1,24 @@
-import React from "react";
+import { filter } from "dom-helpers";
+import React, { useState } from "react";
 import JSONDATA from '../assets/MOCK_DATA.json'
 import { ButtonBack } from "../components/ButtonBackComponent";
 
 export default function Vocabulario() {
+    const [searchTerm, setSearchTerm] = useState('')
     return ( <div>
-        <input type="text" placeholder="Buscar..."></input>
+        <input type="text" placeholder="Buscar..." onChange={event => {setSearchTerm(event.target.value)}}></input>
 
-        {JSONDATA.map((val,key) => {
+        {JSONDATA.filter((val) => {
+            if(searchTerm == ""){
+                return val;
+            }else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+                return val;
+            }
+        }).map((val,key) => {
             return <div>
                 {
                     <div> 
-                         <button>val.first_name</button>
+                         <button>{val.first_name}</button>
                     </div>
                    
                 }
