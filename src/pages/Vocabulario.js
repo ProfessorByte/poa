a031502f3fb1b12e7c4af3e70776e70df7e9846c
mary173 <mariananatv@gmail.com>
2021-11-30 20:27:01 -0400

    fondo fixed

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -7,50 +7,49 @@ import { MDBCol, MDBIcon } from "mdbreact";
 export default function Vocabulario() {
     const [searchItem, setSearchItem] = useState("");
     return (
-        <div>
+        <div className="fondoVocabulario">
             <Header />
-            <div className="fondoVocabulario">
-                <div className="content-wrap mb-3">
-                    <div className="container">
-                        <div className="row mt-3">
-                            <div className="col">
-                                <h1>Vocabulario</h1>
-                            </div>
-                        </div>
-                        <div class="row mb-5">
-                            <MDBCol md="12">
-                                <div className="input-group md-form form-sm form-lg form-1 pl-0 ancho">
-                                    <div className="input-group-prepend">
-                                        <span
-                                            className="input-group-text purple "
-                                            id="basic-text1"
-                                        >
-                                            <MDBIcon
-                                                className="text-white"
-                                                icon="search"
-                                            />
-                                        </span>
-                                    </div>
-                                    <input
-                                        className="form-control form-control-lg "
-                                        type="text"
-                                        id="inputID"
-                                        placeholder="Buscar"
-                                        aria-label="Search"
-                                        onChange={(event) => {
-                                          setSearchItem(event.target.value);
-                                        }}
-                                    />
-                                </div>
-                            </MDBCol>
-                        </div>
-                        <div>
-                            <FrameVocabulario searchTerm={searchItem} />
+            <div className="content-wrap mb-3">
+                <div className="container">
+                    <div className="row mt-3">
+                        <div className="col">
+                            <h1>Vocabulario</h1>
                         </div>
                     </div>
+                    <div class="row mb-5">
+                        <MDBCol md="12">
+                            <div className="input-group md-form form-sm form-lg form-1 pl-0 ancho">
+                                <div className="input-group-prepend">
+                                    <span
+                                        className="input-group-text purple "
+                                        id="basic-text1"
+                                    >
+                                        <MDBIcon
+                                            className="text-white"
+                                            icon="search"
+                                        />
+                                    </span>
+                                </div>
+                                <input
+                                    className="form-control form-control-lg "
+                                    type="text"
+                                    id="inputID"
+                                    placeholder="Buscar"
+                                    aria-label="Search"
+                                    onChange={(event) => {
+                                        setSearchItem(event.target.value);
+                                    }}
+                                />
+                            </div>
+                        </MDBCol>
+                    </div>
+                    <div>
+                        <FrameVocabulario searchTerm={searchItem} />
+                    </div>
                 </div>
-                <FooterVocabulario />
             </div>
+            <FooterVocabulario />
         </div>
+
     );
 }
