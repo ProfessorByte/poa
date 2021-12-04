0618bb52d7f18f7d9913e6209319780d844363f1
Alejandroxp011 <87455252+Alejandroxp011@users.noreply.github.com>
2021-12-03 23:16:21 -0400

    cambio a vocabulario

--- src/pages/Vocabulario.js
+++ src/pages/Vocabulario.js
@@ -1,101 +1,99 @@
-import React, { useState, useEffect } from "react";
-import Header from "../components/HeaderVideos&Biblio";
-import "../css/Vocabulario.css";
-import FooterVocabulario from "../components/FooterRepositorio";
-import FrameVocabulario from "../components/Frame_Vocabulario";
-import { MDBCol, MDBIcon } from "mdbreact";
-import { ModalAdministrarVocabulario } from "../components/ModalAdministrarVocabulario";
-import { getEstadosNivs, getVocabularioQuery } from "../server/api";
-import { useFirestoreCollectionData, useSigninCheck } from "reactfire";
-import DropdownVocabulario from "../components/DropdowOrden";
+import React, { useState, useEffect } from 'react'
+import Header from '../components/HeaderVideos&Biblio'
+import '../css/Vocabulario.css'
+import FooterVocabulario from '../components/FooterRepositorio'
+import FrameVocabulario from '../components/Frame_Vocabulario'
+import { MDBCol, MDBIcon } from 'mdbreact'
+import { ModalAdministrarVocabulario } from '../components/ModalAdministrarVocabulario'
+import { getEstadosNivs, getVocabularioQuery } from '../server/api'
+import { useFirestoreCollectionData, useSigninCheck } from 'reactfire'
+import DropdownVocabulario from '../components/DropdowOrden'
 export default function Vocabulario() {
-  const [searchItem, setSearchItem] = useState("");
-  const [allowManage, setAllowManage] = useState(false);
-  const { status: statusUser, data: signInCheckResult } = useSigninCheck();
+  const [searchItem, setSearchItem] = useState('')
+  const [allowManage, setAllowManage] = useState(false)
+  const { status: statusUser, data: signInCheckResult } = useSigninCheck()
   const [ordener, setOrdener] = useState(false)
 
-  let [listVocabulario, setListVocabulario] = useState([]);
+  let [listVocabulario, setListVocabulario] = useState([])
   const { status, data: cards } = useFirestoreCollectionData(
-    getVocabularioQuery()
-  );
+    getVocabularioQuery(),
+  )
 
   useEffect(() => {
-    if (status !== "loading") {
-      setListVocabulario(cards);
+    if (status !== 'loading') {
+      setListVocabulario(cards)
     }
-  }, [status, cards]);
+  }, [status, cards])
 
   const getUserData = async () => {
-    if (statusUser !== "loading" && signInCheckResult.signedIn) {
-      const userDataQuery = await getEstadosNivs(signInCheckResult.user.uid);
+    if (statusUser !== 'loading' && signInCheckResult.signedIn) {
+      const userDataQuery = await getEstadosNivs(signInCheckResult.user.uid)
       userDataQuery.forEach((doc) => {
-        if (doc.data().role === "admin") {
-          setAllowManage(true);
+        if (doc.data().role === 'admin') {
+          setAllowManage(true)
         }
-      });
+      })
     }
-  };
+  }
 
   useEffect(() => {
-    getUserData();
-  }, [statusUser]);
+    getUserData()
+  }, [statusUser])
 
-  const modalId = "modal-vocabulario";
+  const modalId = 'modal-vocabulario'
 
-  function ordenarAlfabeticamente(){
-    if(status!=="loading"){
-      cards.sort((a , b) => {
+  function ordenarAlfabeticamente() {
+    if (status !== 'loading') {
+      cards.sort((a, b) => {
         if (a.titulo > b.titulo) return 1
         if (a.titulo < b.titulo) return -1
       })
-      setOrdener(!ordener);
+      setOrdener(!ordener)
     }
   }
 
-  
-  function ordenarAlfabeticamenteDes(){
-    if(status!=="loading"){
-      cards.sort((a , b) => {
+  function ordenarAlfabeticamenteDes() {
+    if (status !== 'loading') {
+      cards.sort((a, b) => {
         if (a.titulo < b.titulo) return 1
         if (a.titulo > b.titulo) return -1
       })
-      setOrdener(!ordener);
+      setOrdener(!ordener)
     }
   }
 
-
-  function QueOrden(orden1,orden2){
-    console.log(orden1,orden2)
-    if(orden1==="alfabeticamente"){
-      if(orden2==="ascendente"){
-        ordenarAlfabeticamente() 
-      }else{
+  function QueOrden(orden1, orden2) {
+    console.log(orden1, orden2)
+    if (orden1 === 'alfabeticamente') {
+      if (orden2 === 'ascendente') {
+        ordenarAlfabeticamente()
+      } else {
         ordenarAlfabeticamenteDes()
       }
     }
   }
 
-    return (
-      <>
+  return (
+    <>
       <ModalAdministrarVocabulario
-      modalId={modalId}
-      listVocabulario={listVocabulario}
-      currentUserId={
-        statusUser !== "loading" && signInCheckResult.signedIn
-          ? signInCheckResult.user.uid
-          : ""
-      }
-    />
-        <div className="fondoVocabulario">
-            <Header />
-            <div className="content-wrap mb-3">
-                <div className="container">
-                    <div className="row mt-3">
-                        <div className="col">
-                            <h1>Vocabulario</h1>
-                        </div>
-                    </div>
-                    {statusUser !== "loading" && allowManage && (
+        modalId={modalId}
+        listVocabulario={listVocabulario}
+        currentUserId={
+          statusUser !== 'loading' && signInCheckResult.signedIn
+            ? signInCheckResult.user.uid
+            : ''
+        }
+      />
+      <div className="fondoVocabulario">
+        <Header />
+        <div className="content-wrap mb-3">
+          <div className="container">
+            <div className="row mt-3">
+              <div className="col">
+                <h1>Vocabulario</h1>
+              </div>
+            </div>
+            {statusUser !== 'loading' && allowManage && (
               <div className="row">
                 <button
                   className="col-auto m-3 btn btn-primary"
@@ -106,41 +104,42 @@ export default function Vocabulario() {
                 </button>
               </div>
             )}
-                    <div class="row mb-5">
-                        <MDBCol md="12">
-                            <div className="input-group md-form form-sm form-lg form-1 pl-0 ancho">
-                                <div className="input-group-prepend">
-                                    <span
-                                        className="input-group-text purple-vocabulario "
-                                        id="basic-text1"
-                                    >
-                                        <MDBIcon
-                                            className="text-white"
-                                            icon="search"
-                                        />
-                                    </span>
-                                </div>
-                                <input
-                                    className="form-control form-control-lg "
-                                    type="text"
-                                    id="inputID"
-                                    placeholder="Buscar"
-                                    aria-label="Search"
-                                    onChange={(event) => {
-                                        setSearchItem(event.target.value);
-                                    }}
-                                />
-                                <DropdownVocabulario funcionOrden={QueOrden}/>
-                            </div>
-                        </MDBCol>
-                    </div>
-                    <div>
-                        <FrameVocabulario searchTerm={searchItem} listVocabulario={listVocabulario} status={status}/>
-                    </div>
+            <div class="row mb-5">
+              <MDBCol md="12">
+                <div className="input-group md-form form-sm form-lg form-1 pl-0 ancho">
+                  <div className="input-group-prepend">
+                    <span
+                      className="input-group-text purple-vocabulario "
+                      id="basic-text1"
+                    >
+                      <MDBIcon className="text-white" icon="search" />
+                    </span>
+                  </div>
+                  <input
+                    className="form-control form-control-lg "
+                    type="text"
+                    id="inputID"
+                    placeholder="Buscar"
+                    aria-label="Search"
+                    onChange={(event) => {
+                      setSearchItem(event.target.value)
+                    }}
+                  />
+                  <DropdownVocabulario funcionOrden={QueOrden} />
                 </div>
+              </MDBCol>
+            </div>
+            <div>
+              <FrameVocabulario
+                searchTerm={searchItem}
+                listVocabulario={listVocabulario}
+                status={status}
+              />
             </div>
           </div>
-        <FooterVocabulario />
+        </div>
+      </div>
+      <FooterVocabulario />
     </>
-  );
+  )
 }
