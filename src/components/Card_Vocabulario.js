d70e0e0a633ff7a6ca9be821adc237736e65d817
Serboda <sebasm.sw@gmail.com>
2021-12-04 10:34:28 -0400

    Vocabulary "Tema" Added

--- src/components/Card_Vocabulario.js
+++ src/components/Card_Vocabulario.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../css/Vocabulario.css"
 export default function Card_Vocabulario( {
-  titulo,
+  tema,titulo,
   descripcion
 }){ 
 return(
 < div className="container-fluid">
     <div id="bootstrap-overrides" className="card ">
-          <div id="bootstrap-overrides-title" className="card-header  display-6">{titulo}</div>
-          <h6 className="card-text text-white m-3">{descripcion}</h6>
+          <h3 className="card-header text-success">Tema:{tema}</h3>
+          <h4 id="bootstrap-overrides-title" className="card-title ms-3 mt-3">{titulo}</h4>
+          <h6 className="card-text text-white ms-3 mb-3 me-2">{descripcion}</h6>
     </div>
 </div>
 
