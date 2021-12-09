0901e9a494bc7581a74e5729611818578d0a0ce6
IvelizAyala <ayalaiveliz@gmail.com>
2021-12-08 22:14:52 -0400

    Fix: validacion de entradas de videos

--- src/components/ModalAdministrarVideos.js
+++ src/components/ModalAdministrarVideos.js
@@ -128,9 +128,13 @@ export const ModalAdministrarVideos = ({ modalId, listSections }) => {
     let errors = {};
     if (!values.titleVideo) {
       errors.titleVideo = "El título es requerido";
-    } else if (!/^[a-zA-ZÀ-ÿ\s.,!?]{1,40}$/.test(values.titleVideo)) {
+    } else if (!/^[a-zA-ZÀ-ÿ0-9.:,?¿!¡()/\-+"' ]+$/.test(values.titleVideo)) {
       errors.titleVideo =
-        "El título solo puede contener letras, espacios y .,!?";
+      "El título solo puede contener letras, número, espacios o .:,?¿!¡()/+-'";
+    }
+    if (values.titleVideo.length > 70){
+      errors.titleVideo =
+        "El título solo puede contener 70 caracteres";
     }
 
     if (Number(values.sectionId) === -1) {
