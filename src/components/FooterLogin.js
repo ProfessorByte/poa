3b6d3b430abbee3a8764264eb4acfb82f0abb376
Pablo Pardo <pablopardoalcocer@gmail.com>
2021-12-16 18:15:40 -0400

    Change footer phrase

--- src/components/FooterLogin.js
+++ src/components/FooterLogin.js
@@ -8,9 +8,12 @@ export default class Footer extends Component {
         <footer className="page-footer font-small footLogIn pt-4">
           <div className="container-fluid mt-4">
             <div className="row">
-              <p className="col-sm d-flex justify-content-center">
+              {/*<p className="col-sm d-flex justify-content-center">
                 &copy;{new Date().getFullYear()} Fundamentals | Todos los
                 Derechos Reservados
+              </p>*/}
+              <p className="col-sm d-flex justify-content-center">
+                Todo lo que está hecho con el corazón vale el doble.
               </p>
             </div>
             <br />
--- src/components/FooterRepositorio.js
+++ src/components/FooterRepositorio.js
@@ -7,9 +7,12 @@ export default class Footer extends Component {
         <footer className="page-footer font-small fotter-stylerepo pt-4">
           <div className="container-fluid mt-4">
             <div className="row">
-              <p className="col-sm d-flex justify-content-center">
+              {/*<p className="col-sm d-flex justify-content-center">
                 &copy;{new Date().getFullYear()} Fundamentals | Todos los
                 Derechos Reservados
+              </p>*/}
+              <p className="col-sm d-flex justify-content-center">
+                Todo lo que está hecho con el corazón vale el doble.
               </p>
             </div>
             <br />
